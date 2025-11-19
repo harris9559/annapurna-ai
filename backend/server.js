b17d81e8b1c78bed9b0cf44ad1ee8c227171fa12
Harshit Pandey <168077369+harris9559@users.noreply.github.com>
2025-11-19 17:36:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type, Authorization",
+}));
+
 app.use(express.json());
 
 // MongoDB Connection
